2454b9a01c33d59ae288baf249014d5f010ba138
Michal Adamiak <chajr@bluetree.pl>
2013-08-28 18:13:57 +0200

    Change documentation (issue #7) This version introduces multiple enhancements:
    Added missing documentation
    
    Details of additions/deletions below:
    --------------------------------------------------------------
    
    Added:      /BLUE_FRAMEWORK/packages/user/log_class.php
                -- Added --
                   missing documentation

--- BLUE_FRAMEWORK/packages/user/log_class.php
+++ BLUE_FRAMEWORK/packages/user/log_class.php
@@ -7,57 +7,73 @@
  * @subpackage  benchmark
  * @author      Michał Adamiak    <chajr@bluetree.pl>
  * @copyright   chajr/bluetree
- * @version     1.1.0
+ * @version     1.1.1
  * 
  * @todo add session id to code
  * @todo handling of multi hashing password and code
  */
-class log_class {
-    public static function logOn($uid, $opcje, $grupa)
+class log_class
+{
+    /**
+     * set user as logged in, and save his option in session
+     * @param integer $uid
+     * @param string $options user options in eg format 00101100
+     * @param string $group
+     */
+    public static function logOn($uid, $options, $group)
     {
-        $kod                            = self::kod();
-        $_SESSION['log_class']['log']   = TRUE;
-        $_SESSION['log_class']['uid']   = $uid;
-        $_SESSION['log_class']['kod']   = $kod;
-        $_SESSION['log_class']['opcje'] = $opcje;
-        $_SESSION['log_class']['grupa'] = $grupa;
-        $_SESSION['log_class']['czas']  = time() + 60*60;
+        $code                             = self::_code();
+        $_SESSION['log_class']['log']     = TRUE;
+        $_SESSION['log_class']['uid']     = $uid;
+        $_SESSION['log_class']['code']    = $code;
+        $_SESSION['log_class']['options'] = $options;
+        $_SESSION['log_class']['group']   = $group;
+        $_SESSION['log_class']['time']    = time() + 60*60;
     }
 
+    /**
+     * destroy user information in session
+     */
     public static function logOff()
     {
         $_SESSION['log_class'] = array();
         unset($_SESSION['log_class']);
     }
 
+    /**
+     * check that user is logged in
+     * 
+     * @return bool
+     * @throws LibraryException
+     */
     public static function verifyUser()
     {
-        if (!isset($_SESSION['log_class']['log']) ||
-            !isset($_SESSION['log_class']['uid']) ||
-            !isset($_SESSION['log_class']['kod']) ||
-            !isset($_SESSION['log_class']['opcje']) ||
-            !isset($_SESSION['log_class']['grupa']) ||
-            !isset($_SESSION['log_class']['czas']) ||
-            !$_SESSION['log_class']['log']) {
-
+        if (   !isset($_SESSION['log_class']['log'])
+            || !isset($_SESSION['log_class']['uid'])
+            || !isset($_SESSION['log_class']['code'])
+            || !isset($_SESSION['log_class']['options'])
+            || !isset($_SESSION['log_class']['group'])
+            || !isset($_SESSION['log_class']['time'])
+            || !$_SESSION['log_class']['log']
+        ){
             return FALSE;
         } else {
-            if ($_SESSION['log_class']['kod'] == self::code()) {
-                if ($_SESSION['log_class']['opcje']{0} == '0') {
+            if ($_SESSION['log_class']['code'] === self::_code()) {
+                if ($_SESSION['log_class']['options']{0} === '0') {
                     throw new LibraryException('no_reg');
                 }
 
-                if ($_SESSION['log_class']['opcje']{1} == '0') {
+                if ($_SESSION['log_class']['options']{1} === '0') {
                     throw new LibraryException('blocked');
                 }
 
-                if ($_SESSION['log_class']['czas'] < time()) {
+                if ($_SESSION['log_class']['time'] < time()) {
                     return FALSE;
                 }
 
-                $_SESSION['log_class']['czas'] = time() + 60*60;
+                $_SESSION['log_class']['time'] = time() + 60*60;
                 @session_regenerate_id();
-                $_SESSION['log_class']['kod'] = self::code();
+                $_SESSION['log_class']['code'] = self::_code();
 
                 return TRUE;
             } else {
@@ -66,16 +82,20 @@ class log_class {
         }
     }
 
-    private static function code()
+    /**
+     * return special code to verify user
+     * 
+     * @return string
+     */
+    protected static function _code()
     {
         $client     = $_SERVER['HTTP_USER_AGENT'];
         $ip         = $_SERVER['REMOTE_ADDR'];
-        $kod        = hash(
+        $code        = hash(
             'sha256',
-            $client.
-                $ip
+            $client . $ip
         );
 
-        return $kod;
+        return $code;
     }
 }
