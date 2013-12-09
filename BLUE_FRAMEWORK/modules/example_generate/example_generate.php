b4d4a2b95c3ec7734daad2893990e4e8fe349774
Michal Adamiak <chajr@bluetree.pl>
2013-12-09 21:35:13 +0100

    Make full example modules (issue #20)
    Details of additions/deletions below:
    --------------------------------------------------------------
    
    Added:      /BLUE_FRAMEWORK/modules/example_generate/layouts/index.html
                -- Added --
                    added base structure for example with some working examples
    
    Added:      /BLUE_FRAMEWORK/modules/example_generate/lang/example_generate_en-GB.php
                -- Added --
                    english translations for example
    
    Added:      /BLUE_FRAMEWORK/modules/example_generate/lang/example_generate_pl-PL.php
                -- Added --
                    polish translations for example
    
    Modified:   /BLUE_FRAMEWORK/modules/example_generate/example_generate.php
                -- Added --
                    _simpleMarker replace simple marker
                    _arrayMarker replace markers from array
                -- Updated --
                    run to show up some examples
    
    Modified:   /BLUE_FRAMEWORK/elements/css/bootstrap_dark.css
                -- Updated --
                    changed padding for code node

--- BLUE_FRAMEWORK/modules/example_generate/example_generate.php
+++ BLUE_FRAMEWORK/modules/example_generate/example_generate.php
@@ -5,21 +5,48 @@
  * @subpackage  example_generate
  * @author      Michał Adamiak    <chajr@bluetree.pl>
  * @copyright   chajr/bluetree
- * @version     0.1.0
+ * @version     0.2.0
  */
 class example_generate
     extends module_class
 {
-    static $version             = '0.1.0';
+    static $version             = '0.2.0';
     static $name                = 'content generate module example';
     public $requireLibraries    = array();
     public $requireModules      = array();
 
+    /**
+     * initialize module
+     */
     public function run()
     {
+        $this->layout('index');
+        $this->_translate();
 
+        $this->_simpleMarker();
+        $this->_arrayMarker();
     }
 
-    public function runErrorMode(){
+    /**
+     * replace simple marker
+     */
+    protected function _simpleMarker()
+    {
+        $this->generate('marker', '{;lang;content_to_replace;}');
     }
+
+    /**
+     * replace markers from array
+     */
+    protected function _arrayMarker()
+    {
+        $data = [
+            'marker_1' => '{;lang;content_to_replace_1;}',
+            'marker_2' => '{;lang;content_to_replace_2;}',
+            'marker_3' => '{;lang;content_to_replace_3;}',
+        ];
+        $this->generate($data);
+    }
+
+    public function runErrorMode(){}
 }
---
+++ BLUE_FRAMEWORK/modules/example_generate/lang/example_generate_en-GB.php
@@ -0,0 +1,18 @@
+<?php
+$content = array(
+    'simple_generate'                   => 'Replace simple marker',
+    'content_to_replace'                => 'Replaced content',
+    'title'                             => 'Generate content to markers',
+    'content_array'                     => 'Generate content from array',
+    'core_content'                      => 'Generate content to core template',
+    'optionally_marker_replaced'        => 'Optionally marker - show up',
+    'optionally_marker_none'            => 'Optionally marker - removed',
+    'loop_content'                      => 'Generate content in loop',
+    'loop_content_empty'                => 'Generate content in loop when data is empty',
+    'loop_content_nested'               => 'Generate content in loop for nested loops',
+    'loop_content_optional'             => 'Generate content in loop with optionally markers',
+    'session_content'                   => 'Generate content saved in session',
+    'content_to_replace_1'              => 'Replaced content form array 1',
+    'content_to_replace_2'              => 'Replaced content form array 2',
+    'content_to_replace_3'              => 'Replaced content form array 3',
+);
