edc6faa134482100ca8b8f992d26f67024f9ab38
Michal Adamiak <chajr@bluetree.pl>
2013-12-21 17:53:15 +0100

    Make full example modules (issue #20)
    This version introduces multiple enhancements:
    
    added some examples into template usage
    updated translations
    added some new examples
    
    Details of additions/deletions below:
    --------------------------------------------------------------
    
    Added:      /BLUE_FRAMEWORK/modules/example_layout/layouts/directory/template.html
                -- Added --
                    external template for example
    
    Modified:   /BLUE_FRAMEWORK/modules/example_layout/layouts/index.html
                -- Added --
                    added some new examples
    
    Modified:   /BLUE_FRAMEWORK/elements/layouts/examples/examples.html
                -- Added --
                    added some new examples
    
    Modified:   /BLUE_FRAMEWORK/cfg/lang/core_en-GB.php
                -- Updated --
                    translations changes
    
    Modified:   /BLUE_FRAMEWORK/cfg/lang/core_pl-PL.php
                -- Updated --
                    translations changes
    
    Modified:   /BLUE_FRAMEWORK/modules/example_layout/lang/example_layout_en-GB.php
                -- Added --
                    translations to new examples
    
    Modified:   /BLUE_FRAMEWORK/modules/example_layout/lang/example_layout_pl-PL.php
                -- Added --
                    translations to new examples
    
    Modified:   /BLUE_FRAMEWORK/elements/css/styles.css
                -- Added --
                    style to code description element
    
    Removed:    /BLUE_FRAMEWORK/modules/example_layout/layouts/external/external.html
                -- Removed --
                    removed and replaced by: /BLUE_FRAMEWORK/modules/example_layout/layouts/directory/template.html

--- BLUE_FRAMEWORK/modules/example_layout/lang/example_layout_en-GB.php
+++ BLUE_FRAMEWORK/modules/example_layout/lang/example_layout_en-GB.php
@@ -5,4 +5,10 @@ $content = array(
     'external_templates'                => 'Loading some templates into other template',
     'display_class_instance'            => 'Create new instance of display_class in module and display content from them',
     'display_class_operations'          => 'Create new instance of display_class in module and some operations on layout',
+    'external_template'                 => 'Content from external template',
+    'in_module_template'                => 'inside module template',
+    'in_external_template'              => 'inside external template',
+    'loading_template_description'      => 'To use template for module, paste in core template special marker. Create a template for module and in module give name of template file into layout method.',
+    'loading_template_block'            => 'Loading module template to block marker',
+    'loading_block_description'         => 'To load module (or modules) content to block, we must decelerate block and in module deceleration add attribute block with name name of block',
 );
--- BLUE_FRAMEWORK/modules/example_layout/lang/example_layout_pl-PL.php
+++ BLUE_FRAMEWORK/modules/example_layout/lang/example_layout_pl-PL.php
@@ -5,4 +5,10 @@ $content = array(
     'external_templates'                => 'Wczytywanie zewnętrznej templatki do innej templatki',
     'display_class_instance'            => 'Tworzenie nowej instancji display_class w module i wyświetlanie z niej treści',
     'display_class_operations'          => 'Tworzenie nowej instancji display_class w module i dokonywanie operacji na instancji',
+    'external_template'                 => 'Zawartość z zewnętrznej templatki',
+    'in_module_template'                => 'wewnątrz templatki modułu',
+    'in_external_template'              => 'wewnątrz zewnętrznej templatki',
+    'loading_template_description'      => 'Aby użyć templatki dla modułu anleży uzyć specjalnego markera w templatce głównej. Należy stworzyć templatkędla modułu, a w module użyć nazwy pliku owej templatki w metodzie layout.',
+    'loading_template_block'            => 'Wczytywanie templatki modułu do znacznika bloku',
+    'loading_block_description'         => 'Aby wczytać zawartość z modułu (lub kilka modułów) do bloku należy zadeklarować blok, oraz w deklaracji modułu dodać atrybut block z nazwą bloku do którego ma zostać załadowany moduł.',
 );
