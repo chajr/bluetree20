2a2499e94ad1e4b655c373bf4be9c4160416c0e6
Michał Adamiak <chajr@bluetree.pl>
2021-02-06 20:46:49 +0100

    Added base files for docker stack & moved all framework files to source dir

---
+++ source/BLUE_FRAMEWORK/packages/db/mssql_class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * handling of MS sql database
+ *
+ * @category    BlueFramework
+ * @package     db
+ * @subpackage  mssql
+ * @author      Michał Adamiak    <chajr@bluetree.pl>
+ * @copyright   chajr/bluetree
+ * @version     0.1.1
+ */
+class mssql_class
+    extends sql_abstract
+{
+    /**
+     * set default connection and run given query
+     */
+    public function __construct($sql)
+    {
+
+    }
+
+    /**
+     * return data converted to array
+     */
+    public function result()
+    {
+
+    }
+
+    /**
+     * return mysqli_result result object
+     */
+    public function returns()
+    {
+
+    }
+}
